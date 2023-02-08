4b81a6b3843985ed6b207d035bf1104a56360eb8
Paul Funston <paulfunston9@gmail.com>
2023-02-08 14:13:39 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntroToLINQandASPLabs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
